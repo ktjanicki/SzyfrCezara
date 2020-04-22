c2a797be3dc0de9d4d292aaeee40f44517c66661
Krzysztof Janicki <ktjanicki@gmail.com>
2020-04-22 16:54:02 +0200

    regex fixed

--- cesar.js
+++ cesar.js
@@ -6,7 +6,7 @@ const cesar13 = sentence => {
     const result = [];
     const encrypt = index => parseInt((index + 13) % alphabet.length);
 
-    if (!new RegExp(/^[a-zA-Z0-9\\[\]\\|\/?]*$/g).test(sentence)) {
+    if (!new RegExp(/^[a-zA-Z0-9/?]*$/g).test(sentence)) {
         throw new Error('Program obsługuje tylko litery bez polskich znaków i cyfry');
     } else if (sentence === '') {
         throw new Error('Nic nie zostało wpisane');
