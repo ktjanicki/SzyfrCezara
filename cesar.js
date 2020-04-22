c34233284c29f376d7f10b31c59022342ae896b0
Krzysztof Janicki <ktjanicki@gmail.com>
2020-04-22 14:33:54 +0200

    finally version

--- cesar.js
+++ cesar.js
@@ -1,9 +1,16 @@
-const cesar = sentence => {
+
+const cesar13 = sentence => {
     const alphabet = 'abcdefghijklmnopqrstuvwxyz';
     const alphabetArray = alphabet.split('');
     const alphabetBigArray = alphabet.toUpperCase().split('');
     const result = [];
     const encrypt = index => parseInt((index + 13) % alphabet.length);
+
+    if (!new RegExp(/^[a-zA-Z0-9\\[\]\\|\/?]*$/g).test(sentence)) {
+        throw new Error('Program obsługuje tylko litery bez polskich znaków i cyfry');
+    } else if (sentence === '') {
+        throw new Error('Nic nie zostało wpisane');
+    }
 
     sentence.split('').forEach(letter => {
         if (alphabetArray.includes(letter)) {
@@ -18,4 +25,4 @@ const cesar = sentence => {
     return result.join('');
 }
 
-export { cesar };
+export { cesar13 };
--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // Main JavaScript file
 
-import { cesar } from './cesar.js';
+import { cesar13 } from './cesar.js';
 
 document.querySelector('.encryptButton').addEventListener('click', () => {
-    document.querySelector('.message').textContent = cesar(document.querySelector('input').value);
-    document.querySelector('.result').classList.add('show');
+    try {
+        document.querySelector('.message').textContent = cesar13(document.querySelector('input').value);
+        document.querySelector('.result').classList.add('show');
+    } catch (e) {
+        document.querySelector('.message').textContent = e.message;
+        document.querySelector('.result').classList.add('show');
+    }
 });
